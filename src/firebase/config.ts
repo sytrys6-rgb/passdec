fe7fc1c7da9bfb6ce3b475a75bcfe1fb59386f2d
Sytry_s Beatmaker & Compositeur <sytrys6@gmail.com>
2026-03-01 21:07:40 +0000

    Remplace le contenu de src/firebase/config.ts par cette configuration :

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "pass-dc3-40810388-7231e",
-  "appId": "1:879884137222:web:06b258a490a5786bba6487",
-  "apiKey": "AIzaSyB0srI7pYv2yhoueEv53cNSBgzFtTRLkaA",
-  "authDomain": "pass-dc3-40810388-7231e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "879884137222"
+  apiKey: "AIzaSyDGpnmjBx2qoS6szaPtNbAr5gOdWrWfWCQ",
+  authDomain: "pass-dc4-06880870-d7efa.firebaseapp.com",
+  projectId: "pass-dc4-06880870-d7efa",
+  storageBucket: "pass-dc4-06880870-d7efa.firebasestorage.app",
+  messagingSenderId: "830156464467",
+  appId: "1:830156464467:web:2deaa782db5aaa74d8c1a6"
 };
