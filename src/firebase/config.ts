38140372333523c69efcf77ed280dbee7217ebf8
Sytry_s Beatmaker & Compositeur <sytrys6@gmail.com>
2026-03-01 12:27:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "pass-dc3-40810388-7231e",
+  "appId": "1:879884137222:web:06b258a490a5786bba6487",
+  "apiKey": "AIzaSyB0srI7pYv2yhoueEv53cNSBgzFtTRLkaA",
+  "authDomain": "pass-dc3-40810388-7231e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "879884137222"
+};
