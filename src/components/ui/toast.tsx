69c38d09b4509f7334f2b8c1a50340994c21ad1a
Sytry_s Beatmaker & Compositeur <sytrys6@gmail.com>
2026-03-02 15:55:35 +0000

    Il faut que le fond de l'alerte soit en jaune et l'écriture noire

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -33,7 +33,7 @@ const toastVariants = cva(
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
         warning:
-          "warning group border-warning bg-warning text-warning-foreground shadow-xl font-bold",
+          "warning group border-warning bg-warning text-warning-foreground shadow-2xl font-bold",
       },
     },
     defaultVariants: {
@@ -70,7 +70,7 @@ const ToastAction = React.forwardRef<
     {...props}
   />
 ))
-ToastAction.displayName = ToastPrimitives.Action.displayName
+ToastAction.displayName = ToastAction.displayName
 
 const ToastClose = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Close>,
@@ -79,7 +79,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.warning]:text-warning-foreground/50 group-[.warning]:hover:text-warning-foreground",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.warning]:text-warning-foreground group-[.warning]:hover:text-warning-foreground",
       className
     )}
     toast-close=""
@@ -108,7 +108,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn("text-xs font-bold uppercase tracking-wider opacity-90", className)}
+    className={cn("text-xs font-bold uppercase tracking-wider", className)}
     {...props}
   />
 ))
